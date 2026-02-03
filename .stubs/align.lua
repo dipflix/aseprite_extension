0411dcf5d2cb7e565299e9c16bd7e8f40e8f9ef0
dipflix <vakardhuk.dmitriy.03@gmail.com>
2026-02-03 18:15:17 +0200

    Added aseprite lua stubs

--- .stubs/align.lua
+++ .stubs/align.lua
@@ -1,4 +1,13 @@
 ﻿---@meta
 -- Aseprite API stub: https://www.aseprite.org/api/align
----@enum Align
 
+---@enum Align
+Align = {
+  LEFT = 0,
+  CENTER = 0,
+  RIGHT = 0,
+  TOP = 0,
+  BOTTOM = 0,
+}
+
+Align = Align
